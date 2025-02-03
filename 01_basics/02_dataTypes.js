e06b8f25711fa08544be574ec6c3a00247c7d01d
ayushku634 <123640557+ayushku634@users.noreply.github.com>
2025-02-03 07:10:43 +0000

    L5 Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert('hello') : as we are using nodejs, not browser
+
+console.log(3+3); console.log('Ayush');
+// console.log(3+3) console.log('Ayush'); doesn't work, upper one works but not readable
+
+let name = "Ayush"
+let age = 22
+let isLoggedIn = false
+
+// Primitive DataTypes
+
+// number => 2^53, bigint
+// string => "", ''
+// boolean
+// null (dataType and standalone value)
+// undefined
+// symbol
+
+// object
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof undefined)
+
+// type of null is object 
+console.log(typeof null)
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JS
 Repo for Learning Javascript
+
+### Documentations
+##### https://tc39.es/ecma262/
+##### https://developer.mozilla.org/en-US/docs/Web/JavaScript
