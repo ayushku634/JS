fcdcbf5d85ecc7d9ca518020a9f279dec3dab99c
ayushku634 <123640557+ayushku634@users.noreply.github.com>
2025-02-03 10:48:00 +0000

    L8 Comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// AVOID THESE KIND OF COMPARISONS
+
+// equality check == and comparisons > < >= <= work differently. 
+// Comparisons convert null to a number 0
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true (wtf? but here is an issue of conversion in >=)
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0); // all give false values as undefined is clear
+
+console.log("\n");
+console.log("2" == 2)
+console.log("2" === 2)
